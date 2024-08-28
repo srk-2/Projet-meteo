88ab3625e71ed77aa11cada7077242d2f136399a
skr-2 <wendychardey@gmail.com>
2024-08-28 04:43:28 +0000

    first-commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,9 +114,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'scfeab@gmail.com'
-EMAIL_HOST_PASSWORD = 'Wendy@2024'
-DEFAULT_FROM_EMAIL = 'scfeab@gmail.com'
+
 
 
 # Password validation
